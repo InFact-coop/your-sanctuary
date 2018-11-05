e074f6a857e5a7045ac51bff290657a2792658b5
lucymk <lucy.m.king92@gmail.com>
2018-11-05 11:39:31 +0200

    adds react-helmet and crisp script relates #16

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,9 +1,20 @@
 import { hot } from "react-hot-loader"
 import TitleConnected from "./title"
+import { Helmet } from "react-helmet"
 
 const App = () => (
   <div>
-    <TitleConnected />;
+    <Helmet
+      script={[
+        {
+          type: "text/javascript",
+          innerHTML:
+            'window.$crisp=[];window.CRISP_WEBSITE_ID="eb431f6c-af5b-4a5b-8822-b71066070599";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();',
+        },
+      ]}
+    />
+
+    <TitleConnected />
   </div>
 )
 
