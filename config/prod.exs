61bb691d5a73701fac593abdadb316e7cae62b96
lucymk <lucy.m.king92@gmail.com>
2018-11-05 12:39:22 +0200

    configures app for heroku deployment relates #32

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,16 @@ use Mix.Config
 config :your_sanctuary, YourSanctuaryWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "your-sanctuary.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+config :your_sanctuary, YourSanctuary.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,4 +70,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
