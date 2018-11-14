0cc796318d9e56b0888555113fa3fb1bac09fad1
lucymk <lucy.m.king92@gmail.com>
2018-11-14 09:56:18 +0200

    adds tachyons variables and creates app layout for desktop and mobile relates #40 relates #41

--- assets/src/components/Layout.js
+++ assets/src/components/Layout.js
@@ -4,27 +4,57 @@ import styled from "styled-components"
 
 const FlashMessage = styled.p.attrs({
   className: ({ messageColour }) =>
-    `w-100 pv1 bg-light-red tc white bg-${messageColour}`,
+    `w-100 pv1 tc white font-7 bg-${messageColour}`,
 })``
+
+const MainImageWeb = styled.div.attrs({
+  className: "w-50 dn db-ns",
+})`
+  background-image: url("../static/images/image_bg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+`
+
+const MainContent = styled.div.attrs({
+  className:
+    "w-100 vh-100 w-50-ns bg-near-white blue shadow-4-ns z-2 sans-serif",
+})``
+
+const Footer = styled.div.attrs({
+  className: "w-50-ns w-100 bg-blue white font-6 h3 h4-ns",
+})`
+  position: fixed;
+  bottom: 0;
+`
+
+const FooterRight = styled.div.attrs({
+  className: "bg-light-blue w-100 dn db-ns shadow-5 h3-ns",
+})`
+  position: fixed;
+  bottom: 0;
+`
 
 class Layout extends Component {
   render() {
     const { flash, children } = this.props
     return (
-      <main>
-        {flash.info && (
-          <FlashMessage messageColour="green">{flash.info}</FlashMessage>
-        )}
-        {flash.warning && (
-          <FlashMessage messageColour="yellow">{flash.warning}</FlashMessage>
-        )}
-        {flash.error && (
-          <FlashMessage messageColour="red">{flash.error}</FlashMessage>
-        )}
-        <div className="w-100 vh-100 flex">
-          <section className="w-50 bg-blue">{children}</section>
-          <section className="w-50 bg-light-blue" />
-        </div>
+      <main className="flex sans-serif">
+        <MainContent>
+          {flash.info && (
+            <FlashMessage messageColour="green">{flash.info}</FlashMessage>
+          )}
+          {flash.warning && (
+            <FlashMessage messageColour="yellow">{flash.warning}</FlashMessage>
+          )}
+          {flash.error && (
+            <FlashMessage messageColour="red">{flash.error}</FlashMessage>
+          )}
+          {children}
+          <Footer />
+        </MainContent>
+        <MainImageWeb>
+          <FooterRight />
+        </MainImageWeb>
       </main>
     )
   }
