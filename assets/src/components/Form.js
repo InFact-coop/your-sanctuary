b3bc553d3cab2d9aa1a6e83e39d6140464773ebe
lucymk <lucy.m.king92@gmail.com>
2018-11-15 22:55:10 +0200

    adds 'forgot code' link to Login page

--- assets/src/components/Form.js
+++ assets/src/components/Form.js
@@ -5,6 +5,7 @@ export const Input = styled.input.attrs({
   className:
     "b--blue ba font-6 bg-near-white br-pill pa2 w5 tc black-50 outline-0",
 })`
+  height: 45px;
   ::placeholder {
     color: var(--black-50);
   }
--- assets/src/pages/Login.js
+++ assets/src/pages/Login.js
@@ -1,5 +1,6 @@
 import { Component } from "react"
 import { connect } from "react-redux"
+import { Link } from "react-router-dom"
 
 import { signIn } from "../state/actions/auth"
 import { setErrorFlash } from "../state/actions/flash"
@@ -10,7 +11,7 @@ import { Input } from "../components/Form"
 
 const SubmitButton = RoundButton.withComponent("button")
 
-class SignUp3 extends Component {
+class Login extends Component {
   state = {
     code: "",
   }
@@ -35,13 +36,21 @@ class SignUp3 extends Component {
           If you have spoken to us before please enter your code below
         </BodyText>
         <form onSubmit={this.onSubmit}>
-          <div className="flex flex-column flex-row-ns items-center mb4">
-            <Input
-              onChange={this.onCodeChange}
-              type="text"
-              placeholder="Your code"
-              className="mb4 mb0-ns mr3-ns"
-            />
+          <div className="flex flex-column flex-row-ns items-center items-start-ns justify-center mb4">
+            <div className="flex flex-column">
+              <Input
+                onChange={this.onCodeChange}
+                type="text"
+                placeholder="Your code"
+                className="mb4 mb0-ns mr3-ns"
+              />
+              <Link
+                to="/forgot-code"
+                className="font-7 blue tc-ns mt2-ns mb4 mb0-ns"
+              >
+                I forgot my code
+              </Link>
+            </div>
             <SubmitButton type="submit" />
           </div>
         </form>
@@ -53,4 +62,4 @@ class SignUp3 extends Component {
 export default connect(
   null,
   { signIn, setErrorFlash }
-)(SignUp3)
+)(Login)
